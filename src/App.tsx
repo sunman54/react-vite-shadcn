f40db7cc4b65918cce29be4bd97403b3abf9cacb
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2023-12-30 12:06:25 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Button>Click me</Button>
+
+    </main>
+  )
+}
